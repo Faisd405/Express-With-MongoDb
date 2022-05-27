793674eee075bf6ad7be18044ebc1e62dce9c0ec
Faisd405 <faisd405@gmail.com>
2022-05-27 17:07:00 +0700

    route

--- app.js
+++ app.js
@@ -29,12 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.options('*', cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 
 // Routes
 // app.use('/api/example', auth, exampleRouter);
